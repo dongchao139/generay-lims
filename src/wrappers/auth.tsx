626b6e619877cb0341fde10b745dfdcee7ba515b
donnchao <donnchao@outlook.com>
2021-01-17 15:43:57 +0800

    auth

---
+++ src/wrappers/auth.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Redirect } from 'umi';
+
+const Auth: React.FC = (props) => {
+    const isLogin = true;
+    if (isLogin) {
+        return <div>{props.children}</div>
+    } else {
+        return <Redirect to='/login' />
+    }
+}
+export default Auth;
