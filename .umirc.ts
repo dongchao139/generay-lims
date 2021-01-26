7c9b41549f175dd11cee87af619950cbdc872659
donnchao <donnchao@outlook.com>
2021-01-26 22:39:28 +0800

    arrange files

--- .umirc.ts
+++ .umirc.ts
@@ -12,12 +12,12 @@ export default defineConfig({
         {
           exact: false, path: '/pages', component: '@/layouts/index',
           routes: [
-            { exact: true, path: '/pages/index', component: '@/pages/index' },
-            { exact: true, path: '/pages/Option1', component: '@/pages/Option1' },
-            { exact: true, path: '/pages/Option1Detail', component: '@/pages/Option1Detail' },
+            { exact: true, path: '/pages/index', component: '@/pages/index/index' },
+            { exact: true, path: '/pages/Option1', component: '@/pages/Option1/Option1' },
+            { exact: true, path: '/pages/Option1Detail', component: '@/pages/Option1/Option1Detail' },
             { exact: true, path: '/pages/Option2', component: '@/pages/Option2' },
             { exact: true, path: '/pages/Option3', component: '@/pages/Option3' },
-            { component: '@/pages/404' },
+            { component: '@/pages/404/404' },
           ],
           wrappers: [
             '@/wrappers/auth'
